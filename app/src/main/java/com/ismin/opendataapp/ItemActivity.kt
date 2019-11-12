c460055907ca6d29ceb498ee87365220fe72e687
Hugo <hugo.serieys@hotmail.fr>
2019-11-12 14:21:30 +0100

    Added all fragments and activities

---
+++ app/src/main/java/com/ismin/opendataapp/ItemActivity.kt
@@ -0,0 +1,12 @@
+package com.ismin.opendataapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+    }
+}
